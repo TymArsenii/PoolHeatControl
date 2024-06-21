0ad981f2d95675b2cf411b293ec32026c566d898
Arsenii <137112649+TymArsenii@users.noreply.github.com>
2024-06-21 14:34:26 +0300

    Update -.ino

--- firmware/Wi-Fi/-.ino
+++ firmware/Wi-Fi/-.ino
@@ -2,12 +2,12 @@
 #include <ESP8266HTTPClient.h>
 #include <GSON.h>
 
-#define SSID "i20"
-#define PASS "yanatarsnazsof5"
+#define SSID "---"
+#define PASS "---"
 
 //#include <LiquidCrystal_I2C.h>
 //LiquidCrystal_I2C lcd(0x27, 20, 4);
-
+ 
 #include <GyverDS18Array.h>
 
 #define SENSORS_AMOUNT 1
@@ -22,6 +22,8 @@ uint64_t addr[] =
 };
 GyverDS18Array ds(12, addr, SENSORS_AMOUNT);
 
+bool test_mode=false; //тестовий режим
+
 uint32_t request_temp_timer;
 uint32_t blink_timer;
 uint32_t wifi_connect_timer;
@@ -31,15 +33,16 @@ uint32_t wifi_disconnect_timer;
 uint32_t wifi_check_timer;
 
 float temps_arr[SENSORS_AMOUNT+1]={30.00, 22.00}; // default temps | id1-output water sensor; id2-input water sensor 
-bool curr_state=false; // true-on; false-off
+bool curr_state=false; // true-pumping; false-idle
 bool blink_status;
 float en_water_temp=40.0, dis_water_temp=29.0;
 
 float temp_generator=0.0;
 int time_spent_h, time_spent_m, time_spent_s;
 
-bool wifi_connected=false;
-bool wifi_connected_imid=false;
+uint32_t wifi_connection_timer; //часова мітка старту спроби приєднання до wifi
+bool wifi_connected=false; //маркер наявності активного wifi зʼєднання протягом N сек 
+bool wifi_connected_imid=false; //маркер наявності активного wifi зʼєднання без урахування часу
 
 void IRAM_ATTR timer1_interrupt()
 {
@@ -104,6 +107,8 @@ void setup()
     }
     iters++;
   }
+  //--
+
   if(connection_succesful) 
   {
     //lcd.setCursor(1, 1);
@@ -124,148 +129,208 @@ void setup()
   }
   //lcd.clear();
 }
+
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+
 void loop()
 {
-  if(WiFi.status()!=WL_CONNECTED && wifi_connected && wifi_connected_imid) 
+  // wifi_connected - маркер наявності активного wifi зʼєднання протягом N сек 
+  // wifi_connected_imid - маркер наявності активного wifi зʼєднання без урахування часу
+
+  if(WiFi.status()==WL_CONNECTED)
   {
-    wifi_connected=false;
-    wifi_connected_imid=false;
-    WiFi.begin(SSID, PASS);
-    wifi_check_timer=millis();
-  } 
-  else 
-  {
-    wifi_connected_imid=true;
-    if(millis()-wifi_check_timer>=2000)
+    if (!wifi_connected_imid || !wifi_connected)
     {
-      wifi_connected=true;
+      wifi_connected_imid=true;
+      if(millis()-wifi_check_timer>=2000)
+      {
+        wifi_connected=true;
+      }
     }
   }
-  if(millis()-blink_timer>=1000)
+  else
+  {
+    //між спробами приєднання таймаут 30 сек
+    if (millis()-wifi_connection_timer>=30000)
+    {
+      wifi_connection_timer=millis(); //часова мітка старту спроби приєднання до wifi
+      wifi_connected=false;
+      wifi_connected_imid=false;
+      WiFi.begin(SSID, PASS);
+      wifi_check_timer=millis();
+    }    
+  } 
+
+  /*if(millis()-blink_timer>=1000)
   {
     blink_timer=millis();
     blink_status=!blink_status;
     //lcd.setCursor(19, 3);
     //lcd.print(blink_status ? "A" : "T");
-  }
+  }*/
 
-  if(millis()-check_server_timer>=1000 && wifi_connected)
+  // wifi_connected - маркер наявності активного wifi зʼєднання протягом N сек
+  if (wifi_connected)
   {
-    check_server_timer=millis();
-
-    WiFiClient client_get;
-    HTTPClient http_get;
-    http_get.begin(client_get, "http://exch.com.ua/pool_target_temps.txt");
-    int httpCode=http_get.GET();
-    
-    String payload;
-    if(httpCode>0)
+    if(millis()-check_server_timer>=5000)
     {
-      // If the response was successful, print the response payload
-      payload=http_get.getString();
-      Serial.println(payload);
-    }
-    else
-    {
-      Serial.println("Error on HTTP request");
-    }
-    http_get.end();
+      check_server_timer=millis();
 
-    gson::Parser p(10);
-    p.parse(payload);
-
-    dis_water_temp=p["disable_temp"];
-    en_water_temp=p["enable_temp"];
-
-    Serial.println("==== READ ====");
-    Serial.print("dis_water_temp=");
-    Serial.println(dis_water_temp);
-    Serial.print("en_water_temp=");
-    Serial.println(en_water_temp);
-  }
-
-  WiFiClient client;
-  HTTPClient http;
-  http.begin(client, "http://exch.com.ua/Pool/PoolHeatMonitor.php");
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  if(millis()-request_temp_timer>=1500)
-  {
-    request_temp_timer=millis();
-    for(int id=0; id<ds.amount(); id++)
-    {
-      if(ds.readTemp(id) || 1==1)
+      WiFiClient client_get;
+      HTTPClient http_get;
+      http_get.begin(client_get, "http://exch.com.ua/pool_target_temps.txt");
+      int httpCode=http_get.GET();
+      
+      String payload;
+      if(httpCode == HTTP_CODE_OK)
       {
-        float gotten_temp=ds.getTemp();
-        temps_arr[id]=gotten_temp;
-        if(wifi_connected)
-        {
-          temp_generator+=0.1;
-          if(temp_generator>50) temp_generator=0.0;
-          temps_arr[id]=gotten_temp;
-          temps_arr[id]=temp_generator;
-          String data_post="temp=";
-          data_post+=temps_arr[id];
-          data_post+="&dis_temp=";
-          data_post+=dis_water_temp;
-          data_post+="&en_temp=";
-          data_post+=en_water_temp;
-            
-          int httpCode = http.POST(data_post);
-        }
-        //Serial.println(data_post);
+        // If the response was successful, print the response payload
+        payload="";
+        payload=http_get.getString();
+        Serial.println(payload);
+        // --
+        gson::Parser p(10);
+        p.parse(payload);
+
+        dis_water_temp=p["disable_temp"];
+        en_water_temp=p["enable_temp"];
+
+        Serial.println("==== READ ====");
+        Serial.print("dis_water_temp=");
+        Serial.println(dis_water_temp);
+        Serial.print("en_water_temp=");
+        Serial.println(en_water_temp);                
       }
+      else
+      {
+        Serial.println("Error on HTTP request");
+      }
+      http_get.end();
     }
-    ds.requestTemp();
 
-    // <- sensor processing
-    // relay processing ->
+    // ----------------------------------------------------------------
 
-    /*
-    float delta=temps_arr[0]-temps_arr[1];
-    if((delta>=(MIN_WATER_DELTA+0.5) && temps_arr[0]>=MIN_OUTPUT_TEMP) && !curr_state)
+
+    if(millis()-request_temp_timer>=3000)
     {
-      digitalWrite(4, 0); // turn on. 0 is on, it's not a mistake!
-      curr_state=1;
+      WiFiClient client;
+      HTTPClient http;
+      http.begin(client, "http://exch.com.ua/Pool/PoolHeatMonitor.php");
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      //--
+      request_temp_timer=millis();
+      for(int id=0; id<ds.amount(); id++)
+      {
+        if(ds.readTemp(id) || test_mode)
+        {
+          float gotten_temp=ds.getTemp();
+          //--
+          if (test_mode)
+          {
+            temp_generator+=0.1;
+            if(temp_generator>50) temp_generator=0.0;
+            temps_arr[id]=temp_generator;
+          }
+          //--
+          temps_arr[id]=gotten_temp;
+          //Serial.println(data_post);
+        }
+      } //for
+      //--
+      bool state;
+      if(temps_arr[0]>=en_water_temp && !curr_state)
+      {
+        state=true;
+      }
+      else if(temps_arr[0]<=dis_water_temp && curr_state)
+      {
+        state=false;
+      }
+      String data_post="temp=";
+      data_post+=temps_arr[0];
+      data_post+="&dis_temp=";
+      data_post+=dis_water_temp;
+      data_post+="&en_temp=";
+      data_post+=en_water_temp;
+      data_post+="&state=";
+      data_post+=state;
+        
+      int httpCode = http.POST(data_post);
+      http.end();
+      //--
+      ds.requestTemp();
 
-      Timer1.restart();
-      time_spent_s = 0;
-      time_spent_m = 0;
-      time_spent_h = 0;
+      // <- sensor processing
+      // relay processing ->
+
+      /*
+      float delta=temps_arr[0]-temps_arr[1];
+      if((delta>=(MIN_WATER_DELTA+0.5) && temps_arr[0]>=MIN_OUTPUT_TEMP) && !curr_state)
+      {
+        digitalWrite(4, 0); // turn on. 0 is on, it's not a mistake!
+        curr_state=1;
+
+        Timer1.restart();
+        time_spent_s = 0;
+        time_spent_m = 0;
+        time_spent_h = 0;
+      }
+      else if((delta<MIN_WATER_DELTA || temps_arr[0]<(MIN_OUTPUT_TEMP-0.5)) && curr_state)
+      {
+        digitalWrite(4, 1);
+        curr_state=0;
+
+        Timer1.restart();
+        time_spent_s = 0;
+        time_spent_m = 0;
+        time_spent_h = 0;
+      }
+      */
     }
-    else if((delta<MIN_WATER_DELTA || temps_arr[0]<(MIN_OUTPUT_TEMP-0.5)) && curr_state)
+
+            
+  } //if (wifi_connected) 
+  else
+  {    
+    if(millis()-request_temp_timer>=3000)
     {
-      digitalWrite(4, 1);
-      curr_state=0;
-
-      Timer1.restart();
-      time_spent_s = 0;
-      time_spent_m = 0;
-      time_spent_h = 0;
-    }
-    */
-
-    if(temps_arr[0]>=en_water_temp && !curr_state)
-    {
-      digitalWrite(4, 0); // turn on. 0 is on, it's not a mistake!
-      curr_state=1;
-
-      time_spent_s = 0;
-      time_spent_m = 0;
-      time_spent_h = 0;
-    }
-    else if(temps_arr[0]<=dis_water_temp && curr_state)
-    {
-      digitalWrite(4, 1);
-      curr_state=0;
-
-      time_spent_s = 0;
-      time_spent_m = 0;
-      time_spent_h = 0;
-    }
+      request_temp_timer=millis();
+      //--
+      for(int id=0; id<ds.amount(); id++)
+      {
+        if(ds.readTemp(id) || test_mode)
+        {
+          float gotten_temp=ds.getTemp(); 
+        }
+      }
+      ds.requestTemp();
+    }   
   }
 
-  if(wifi_connected) http.end();
+
+  // Керування реле -->>
+  if(temps_arr[0]>=en_water_temp && !curr_state)
+  {
+    digitalWrite(4, 0); // turn on. 0 is on, it's not a mistake!
+    curr_state=1;
+
+    time_spent_s = 0;
+    time_spent_m = 0;
+    time_spent_h = 0;
+  }
+  else if(temps_arr[0]<=dis_water_temp && curr_state)
+  {
+    digitalWrite(4, 1);
+    curr_state=0;
+
+    time_spent_s = 0;
+    time_spent_m = 0;
+    time_spent_h = 0;
+  }
+  // Керування реле --<<
+
 
   // GUI/UI ->
   /*
